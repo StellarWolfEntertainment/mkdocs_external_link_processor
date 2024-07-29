9efbc107eca044f41fdd73df3e0e888b6291d3f5
StellarWolfEntertainment <stellarwolfentertainment@gmail.com>
2024-07-29 07:52:24 -0600

    Added workflow

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='mkdocs-external-link-processor',
     version='0.1.0',
-    description='A MkDocs plugin that adds a CSS class to external links',
+    description='A MkDocs plugin that adds a CSS attributes to external links',
     author='Raistlin Wolfe',
     author_email='jdoonan61@gmail.com',
     url='https://github.com/StellarWolfEntertainment/mkdocs-external-link-processor',
@@ -16,5 +16,18 @@ setup(
         'mkdocs.plugins': [
             'mkdocs_external_link_processor=mkdocs_external_link_processor.mkdocs_external_link_processor:MkdocsExternalLinkProcessor',
         ],
-    }
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
