8f4f3a7fec3852c7ecfc54c40140ab1691549a2a
StellarWolfEntertainment <stellarwolfentertainment@gmail.com>
2024-07-29 04:57:36 -0600

    Enhance MkdocsExternalLinkProcessor with customizable attributes and 'www' prefix support
    - Added `target` and `rel` options to customize link attributes, with default values '_blank' and ['noopener', 'noreferrer'].
    - Updated the plugin to recognize and process links starting with 'www' as external, in addition to 'http://' and 'https://'.
    - Adjusted `on_page_content` method to apply the specified attributes to external links.
    - Ensured that only valid external links receive the custom CSS class and attributes.

--- mkdocs_external_link_processor/mkdocs_external_link_processor.py
+++ mkdocs_external_link_processor/mkdocs_external_link_processor.py
@@ -6,15 +6,17 @@ class MkdocsExternalLinkProcessor(BasePlugin):
     A MkDocs plugin that adds a CSS class to external links in the page content.
 
     This plugin processes the HTML content of MkDocs pages and appends a specified
-    class to external links (links starting with 'http://' or 'https://').
+    class to external links (links starting with 'http://', 'https://', or 'www').
 
     Attributes:
         class_name (str): The CSS class to add to external links. Defaults to 'external'.
+        target (str): The target attribute for external links. Defaults to '_blank'.
+        rel (list): The rel attribute for external links. Defaults to ['noopener', 'noreferrer'].
     """
     
     def __init__(self, *args, **kwargs):
         """
-        Initializes the plugin and retrieves the CSS class name from the configuration.
+        Initializes the plugin and retrieves the CSS class name and attributes from the configuration.
 
         Args:
             *args: Variable length argument list.
@@ -22,6 +24,8 @@ class MkdocsExternalLinkProcessor(BasePlugin):
         """
         super().__init__(*args, **kwargs)
         self.class_name = self.config.get('external_link_class_name', 'external')
+        self.target = self.config.get('link_target', '_blank')
+        self.rel = self.config.get('link_rel', ['noopener', 'noreferrer'])
 
     def on_page_content(self, html: str, page, config, files) -> str:
         """
@@ -43,13 +47,16 @@ class MkdocsExternalLinkProcessor(BasePlugin):
         soup = BeautifulSoup(html, 'html.parser')
         for a_tag in soup.find_all('a', href=True):
             href = a_tag['href']
-            if href.startswith(('http://', 'https://')):
-                if 'external' not in a_tag.get('class', []):
-                    
-                    classes = a_tag.get('class', [])
-                    if not isinstance(classes, list):
-                        classes = []
-                    a_tag['class'] = classes + [self.class_name]
-                a_tag["target"] = '_blank'
-                a_tag["rel"] = ['noopener', 'noreferrer']
+            if not href.startswith(('http://', 'https://', 'www')):
+                continue
+
+            if 'external' not in a_tag.get('class', []):
+                
+                classes = a_tag.get('class', [])
+                if not isinstance(classes, list):
+                    classes = []
+                a_tag['class'] = classes + [self.class_name]
+                
+            a_tag["target"] = self.target
+            a_tag["rel"] = self.rel
         return str(soup)
